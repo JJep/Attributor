a5829219e15ad520b443c573c7862e1ffa40b33d
Jep <xjn377969949@live.com>
2017-02-02 21:23:05 +0800

    AttributorWithLifecycle

--- Attributor/ViewController.m
+++ Attributor/ViewController.m
@@ -10,6 +10,7 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *headline;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 @property (weak, nonatomic) IBOutlet UITextView *body;
 
 @end
@@ -21,6 +22,36 @@
                                   value:sender.backgroundColor
                                   range:self.body.selectedRange];
 }
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self usePreferredFonts];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(preferredFontIsChanged:)
+                                                 name:UIContentSizeCategoryDidChangeNotification
+                                               object:nil];
+}
+
+-(void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIContentSizeCategoryDidChangeNotification
+                                                  object:nil];
+}
+
+- (void) preferredFontIsChanged:(NSNotification *)notification
+{
+    [self usePreferredFonts];
+}
+
+-(void) usePreferredFonts{
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+}
+
+
 
 - (IBAction)outlineBodySelection:(UIButton *)sender
 {
@@ -37,8 +68,15 @@
 
 
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
+    NSMutableAttributedString *title =
+        [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+    [title addAttributes:@{NSStrokeWidthAttributeName : @-3,
+                           NSStrokeColorAttributeName : self.outlineButton.currentTitleColor}
+                   range:NSMakeRange(0, self.outlineButton.currentTitle.length)];
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
